0e6e6acb8cd6eabc4d04d168a90ecb631d6fb3cc
younghun1982 <younghun1982@gmail.com>
2025-11-30 16:45:25 +0900

    fix: update Contact page title to 'Contact test'

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Contact
+            Contact test
           </h1>
         </div>
       </main>
